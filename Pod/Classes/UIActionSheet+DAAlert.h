ebc15ccb6b6bd5d3a5cec4efa8c589a3bb4a9060
MUISEROUX Fritzgerald <f.muiseroux@gmail.com>
2015-05-13 23:54:10 +0200

    Handle more than 10 other actions for UIAlertView and UIActionSheet
    Remove other button limitation and use categories for DAAlertController
    properties in UIAlertView and UIActionSheet

---
+++ Pod/Classes/UIActionSheet+DAAlert.h
@@ -0,0 +1,19 @@
+//
+//  UIActionSheet+DAAlert.h
+//  DAAlertTest
+//
+//  Created by fritzgerald muiseroux on 13/05/15.
+//  Copyright (c) 2015 fritzgerald muiseroux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DAAlertAction;
+@interface UIActionSheet (DAAlert)
+
+@property (copy, nonatomic) DAAlertAction *daCancelAction;
+@property (copy, nonatomic) DAAlertAction *daDestructiveAction;
+@property (copy, nonatomic) NSArray *daOtherActions;
+@property (copy, nonatomic) BOOL (^daValidationBlock)(NSArray *textFields);
+
+@end
